6e41857f928d2a3a060efd8686d0c77cdb369aea
unknown <mowlesar.kathir@gmail.comcd bootcampOmit --git config mowlesar.kathir@gmail.com>
2023-08-05 14:38:32 -0400

    Added array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,46 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'Please enter the title of your project.'
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Please describe your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide any installation instructions.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide any usage information .'
+    },
+    {
+        type: 'input',
+        name: 'Contribution',
+        message: 'Please provide contribution guidlines.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide any test instructions.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license for your application',
+        choices: ['MIT', 'Apache 2.0', 'GNU GPLv3', 'ISC', 'None'],
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
